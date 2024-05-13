063558a42b252b50f5f0bfcdabf7a22467017f08
Łukasz Osiennik <lukasz.osiennik@gmail.com>
2024-05-13 09:05:39 +0200

    Initial project commit

---
+++ SelfDevelopment/SelfDevelopment/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  Created by Łukasz Osiennik on 13/05/2024.
+//  Copyright © 2024 openplainness (https://www.openplainness.com). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SelfDevelopment/SelfDevelopment/SelfDevelopmentApp.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Łukasz Osiennik on 13/05/2024.
+//  Copyright © 2024 openplainness (https://www.openplainness.com). All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SelfDevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
